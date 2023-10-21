bc88cd8e0a9a506b40229b9703344debcdf3fe74
Dan <daniele.d@hotmail.co.uk>
2023-10-21 16:19:22 +0100

    main

---
+++ game_outcome_prediction.py
@@ -0,0 +1,43 @@
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+# Load the dataset
+file_path = '/mnt/data/bubble-trubble-open-cup-jgh9se8qod-players-games.csv'
+df = pd.read_csv(file_path)
+
+# Encoding the categorical variables
+label_encoders = {}  
+for column in df.select_dtypes(include=['object']).columns:
+    le = LabelEncoder()
+    df[column] = le.fit_transform(df[column])
+    label_encoders[column] = le
+
+# Separating the features and the target variable
+X = df.drop(columns=['result'])
+y = df['result']
+
+# Splitting the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Creating the Decision Tree classifier
+clf = DecisionTreeClassifier(random_state=42)
+
+# Training the classifier
+clf.fit(X_train, y_train)
+
+# Predicting the test set results
+y_pred = clf.predict(X_test)
+
+# Calculating the accuracy of the model
+accuracy = accuracy_score(y_test, y_pred)
+
+# Displaying the classification report
+classification_rep = classification_report(y_test, y_pred)
+
+print(f'Accuracy: {accuracy}')
+print('Classification Report:')
+print(classification_rep)
