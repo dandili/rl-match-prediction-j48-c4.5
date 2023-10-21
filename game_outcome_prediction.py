740a0f72f3df17885d78e5fad8ff30f7ac4647b7
Dan <daniele.d@hotmail.co.uk>
2023-10-21 16:19:41 +0100

    Update game_outcome_prediction.py

--- game_outcome_prediction.py
+++ game_outcome_prediction.py
@@ -6,7 +6,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
 # Load the dataset
-file_path = '/mnt/data/bubble-trubble-open-cup-jgh9se8qod-players-games.csv'
+file_path = 'stats.csv'
 df = pd.read_csv(file_path)
 
 # Encoding the categorical variables
